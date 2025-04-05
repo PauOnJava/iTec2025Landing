5840d8c0bea14320162bc91fafa1532900636bd0
PauOnJava <95082435+PauOnJava@users.noreply.github.com>
2025-04-05 15:37:43 +0300

    final of the finals touches

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="w-screen text-white bg-blue-500 p-4 m-0  bottom-0">
+    <footer className="w-screen text-white bg-blue-500 p-4 mt-5 bottom-0">
       <div className="container mx-auto ">
         <div className="flex flex-col items-center">
           <p>© 2025 StudentHub. All rights reserved.</p>
